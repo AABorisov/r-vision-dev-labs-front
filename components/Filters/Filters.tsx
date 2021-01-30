cdf388535d85c6ec93fce02f5ec30628f7835d77
Kozinets Svyatoslav <Kozinets.S.Vi@sberbank.ru>
2021-01-30 23:41:01 +0300

    filter

--- components/Filters/Filters.tsx
+++ components/Filters/Filters.tsx
@@ -1,42 +1,70 @@
-import { Select, Typography, Col, Row } from "antd";
+import React from "react";
+import {Select, Typography, Col, Row} from "antd";
 import Card from "../Cards/Card";
-const { Title } = Typography;
+import {
+  campaigns,
+  identities,
+  industries,
+  malwares,
+  softwares,
+  threatActors
+} from "../../mocks/filter";
 
-const Filters: React.FC = ({ children }) => {
-  const filterTitles = ['Malware', 'Software', 'Identity', 'Threat_actor', 'Campaign', 'Industry'];
-  const filters = filterTitles.map((filterTitle) => {
-    return {
-      title: filterTitle,
-      value: 0,
-      options: [0, 1, 2, 3].map((i) => ({
-        label: `${filterTitle} #${ i }`,
-          value: i
-      }))
-    };
-  });
-  const handleChange = (filterTitle: string) => (value: string|number) => {
-    console.log(`${filterTitle}: ${value}`)
-  }
+const {Title} = Typography;
+
+interface IProps {
+  onFilter: (value: any, fieldName: string) => void
+}
+
+const Filters = ({onFilter}: IProps) => {
+  const filters = [
+    {
+      title: "Malware",
+      options: malwares,
+      onChange: (value: number) => onFilter(value, "malware")
+    },
+    {
+      title: "Software",
+      options: softwares,
+      onChange: (value: number) => onFilter(value, "software")
+    },
+    {
+      title: "Identity",
+      options: identities,
+      onChange: (value: number) => onFilter(value, "identity")
+    },
+    {
+      title: "Threat actor",
+      options: threatActors,
+      onChange: (value: number) => onFilter(value, "threat_actor")
+    },
+    {
+      title: "Campaign",
+      options: campaigns,
+      onChange: (value: number) => onFilter(value, "campaign")
+    },
+    {
+      title: "Industry",
+      options: industries,
+      onChange: (value: number) => onFilter(value, "industry")
+    }
+  ]
+
   return (
-      <Card title="Filters">
-        <Row gutter={[20, 20]}>
-        { filters.map((filter) => {
-          return (
-            <Col key={filter.title} >
-              <Title level={4}>{filter.title}</Title>
-              <Select
-                style={{ width: '200px' }}
-                placeholder="All"
-                onChange={handleChange}
-                options={filter.options}
-              />
-            </Col>
-          );
-        })
-        }
-        </Row>
-        { children }
-      </Card>
+    <Card title="Filters">
+      <Row gutter={[20, 20]}>
+        {filters.map((filter, key) =>
+          <Col key={key}>
+            <Title level={4}>{filter.title}</Title>
+            <Select
+              style={{width: 200}}
+              placeholder="All"
+              options={filter.options}
+              onChange={filter.onChange}/>
+          </Col>
+        )}
+      </Row>
+    </Card>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,35 @@
+import React, {useEffect, useState} from "react";
 import {Typography} from "antd"
 import Wrapper from '../components/Wrapper';
 import Filters from "../components/Filters/Filters";
 import Recognized from "../components/Tables/Recognized/Recognized";
 import Uploaded from "../components/Tables/Uploaded/Uploaded";
-import recognized from "../mocks/recognized"
-import uploaded from "../mocks/uploaded"
+import recognizedMock from "../mocks/recognized"
+import uploadedMock from "../mocks/uploaded"
+import {IRecognizedTableRow, IUploadedTableRow} from "../interfaces";
 
 const {Title} = Typography;
 
 const Home: React.FC = () => {
+  const [filtered, setFiltered] = useState<IRecognizedTableRow[]>([])
+  const [uploaded, setUploaded] = useState<IUploadedTableRow[]>([])
+
+  useEffect(() => {
+    setFiltered(recognizedMock)
+    setUploaded(uploadedMock)
+  }, [])
+
+  const onFilter = (value: any, fieldName: string) => {
+    const filtered = recognizedMock.filter((item: any) => item[fieldName] === value)
+
+    setFiltered(filtered)
+  }
+
   return (
     <Wrapper>
       <Title>Dashboard</Title>
-      <Filters/>
-      <Recognized data={recognized}/>
+      <Filters onFilter={onFilter}/>
+      <Recognized data={filtered}/>
       <Uploaded data={uploaded}/>
     </Wrapper>
   )
