4fea0c287987d01a193075c8388d9221e0730a3c
Kozinets Svyatoslav <Kozinets.S.Vi@sberbank.ru>
2021-01-30 22:47:24 +0300

    add mocks for tables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,20 @@
+import {Typography} from "antd"
 import Wrapper from '../components/Wrapper';
-
-import { Typography } from "antd"
 import Filters from "../components/Filters/Filters";
-import Recognized from "../components/Tables/Recognized";
-import Uploaded from "../components/Tables/Uploaded";
+import Recognized from "../components/Tables/Recognized/Recognized";
+import Uploaded from "../components/Tables/Uploaded/Uploaded";
+import recognized from "../mocks/recognized"
+import uploaded from "../mocks/uploaded"
 
-const { Title } = Typography;
+const {Title} = Typography;
 
 const Home: React.FC = () => {
   return (
     <Wrapper>
       <Title>Dashboard</Title>
       <Filters/>
-      <Recognized />
-      <Uploaded />
+      <Recognized data={recognized}/>
+      <Uploaded data={uploaded}/>
     </Wrapper>
   )
 }
